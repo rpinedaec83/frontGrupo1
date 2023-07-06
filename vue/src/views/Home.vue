000c59674bc97fc19548d8b39676d040a647b654
Manuel <frama9125@gmail.com>
2023-07-05 22:15:30 -0500

    update

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -20,4 +20,124 @@ const { user } = storeToRefs(authStore);
             </div>
         </div>
     </div>
+    <div class="section main-section courses-section">
+        <div class="container">
+            <div class="content">
+                <div class="img-wrapper sponsore-wrapper">
+                    <img src="@/assets/img/png/sponsor.png" alt="">
+                </div>
+                <div class="heading-wrapper heading-is-purple">
+                    <h3 class="heading-tag">Conoce nuestros Programas de Especialización</h3>
+                </div>
+                <div class="cursos-wrapper">
+                    <div class="cursos-wrapper-inner">
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cursos-item">
+                            <div class="curso-inner">
+                                <div class="img-wrapper">
+                                    <img src="@/assets/img/jpg/cursos.jpg" alt="">
+                                </div>
+                                <div class="heading-wrapper">
+                                    <h3 class="heading-tag">Desarrollo Front-End</h3>
+                                </div>
+                                <div class="button-wrapper">
+                                    <a href="#" class="button">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
