09f23dee37fb752ea3d925757020c45dc936c988
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-07-08 02:12:45 -0500

    Se agrego al router el carrito y se cero la vista carrito

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore, useAlertStore } from '@/stores';
 import { Home } from '@/views';
 import { accede } from '@/views';
+import { Carrito } from '@/views';
 import accountRoutes from './account.routes';
 import usersRoutes from './users.routes';
 
@@ -15,31 +16,15 @@ export const router = createRouter({
             path: "/accede",
             component: accede,
             meta: { hideHeaderFooter: true },
-          },
+        },
+        {
+            path: "/carrito",
+            component: Carrito,
+            meta: { hideHeaderFooter: true },
+        },
         { ...accountRoutes },
         { ...usersRoutes },
         // catch all redirect to home page
         { path: '/:pathMatch(.*)*', redirect: '/' }
     ]
 });
-
-
-
-// router.beforeEach(async (to) => {
-//     // clear alert on route change
-//     const alertStore = useAlertStore();
-//     alertStore.clear();
-
-    
-//     // redirect to login page if not logged in and trying to access a restricted page 
-//     const publicPages = ['/account/login', '/account/register'];
-//     const authRequired = !publicPages.includes(to.path);
-//     const authStore = useAuthStore();
-
-    
-//     if (authRequired && !authStore.user) {
-//         authStore.returnUrl = to.fullPath;
-//         return '/account/login';
-//     }
-    
-// });
